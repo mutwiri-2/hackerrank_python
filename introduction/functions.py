1c7f269211fc30200d10965d03d433a6791ea8be
mutwiri-2 <devmutwiri2@gmail.com>
2020-01-30 16:30:45 +0300

    DRY

--- introduction/functions.py
+++ introduction/functions.py
@@ -27,8 +27,6 @@ def is_leap(year):
     if year % 4 == 0:
         if(year % 100 ==0) and (year % 400 ==0):
             leap=True
-    else:
-        leap = False
     return leap
     # Write your logic here
 
