d2ef47e43ca8ef76f3151180928300815dc598cb
mutwiri-2 <devmutwiri2@gmail.com>
2020-01-30 18:58:18 +0300

    sum of the two smallest integers given a list of integers

---
+++ medium_ease/list_comprehensions.py
@@ -0,0 +1,1 @@
+# You are given three integers x,y and z representing the dimensions of a cuboid along with an integer n. You have to print a list of all possible coordinates given by (i,j,k) on a 3D grid where the sum of i + j + k  is not equal to n. Here 0<=i<=x; 0<=j<=y; 0<=k<=z; 
