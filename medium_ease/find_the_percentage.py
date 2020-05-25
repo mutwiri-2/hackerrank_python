1e3280ef07367bfc8108230a9b8029ca9ed343da
mutwiri-2 <devmutwiri2@gmail.com>
2020-05-25 23:08:38 +0300

    add test data

--- medium_ease/find_the_percentage.py
+++ medium_ease/find_the_percentage.py
@@ -30,15 +30,23 @@
 # Sample Output 1
 # 26.50
 
-if __name__ == '__main__':
-    n = int(input())
-    student_marks = {}
-    for _ in range(n):
-        name, *line = input().split()
-        scores = list(map(float, line))
-        student_marks[name] = scores
-    query_name = input()
+# if __name__ == '__main__':
+#     n = int(input())
+#     student_marks = {}
+#     for _ in range(n):
+#         name, *line = input().split()
+#         scores = list(map(float, line))
+#         student_marks[name] = scores
+#     query_name = input()
 
-    marks = student_marks.get(query_name)
-    average = sum(marks) / len(marks)
-    print(format(average, ".2f"))
+student_marks = {
+    'Krishna': [67, 68, 69],
+    'Arjun': [70, 98, 63],
+    'Malika': [52, 56, 60],
+}
+
+query_name = 'Malika'
+
+marks = student_marks.get(query_name)
+average = sum(marks) / len(marks)
+print(format(average, ".2f"))
