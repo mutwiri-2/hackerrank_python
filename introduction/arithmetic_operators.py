372abcd7180440c6f3e481b95220243290335321
mutwiri-2 <devmutwiri2@gmail.com>
2021-01-11 14:43:23 +0300

    add user prompts

--- introduction/arithmetic_operators.py
+++ introduction/arithmetic_operators.py
@@ -4,17 +4,18 @@
 # 2. The second line contains the difference of the two numbers (first - second).
 # 3. The third line contains the product of the two numbers.
 
-a = int(input())
-b = int(input())
+a = int(input("enter any integer: "))
+b = int(input("enter another integer: "))
 
-print(a+b)
-print(a-b)
-print(a*b)
+print("The sum of the integers is: ",a+b)
+print("The difference between the integers is:",a-b)
+print("The product of the integers is:" ,a*b)
 
-# Read two integers and print two lines. The first line should contain integer division. The second line should contain float division
+# Read two integers and print two lines. The first line should contain integer
+# division. The second line should contain float division
 
-c = int(input())
-d = int(input())
+c = int(input("Enter an integer1: "))
+d = int(input("Enter another integer2: "))
 
-print(c//d)  #integer division
-print(c/d)  #float division
+print("integer1 // integer2 equals", c//d)  #integer division
+print("integer1 / integer2 equals ", c/d)  #float division
